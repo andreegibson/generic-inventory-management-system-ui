f263831b12b3060d2618fd24a7e5e93cf03521d8
Andree Gibson <andree.gibson@gmail.com>
2021-05-18 18:52:29 -0500

    Get by Name Improved for List
    Now calling get by name from the UI receives a list of items and displays them in a table. Added Bootstrap, too!

--- src/components/GIMS.vue
+++ src/components/GIMS.vue
@@ -1,40 +1,66 @@
 <template>
   <div class="gims">
-    <h1>{{ msg }}</h1>
-    <p>
-      Description text goes here.
-    </p>
-    <h3>Retrieve an Item from the Item Database</h3>
-    <div class="inputs">
-      <label for="getItemName">Item name:</label>
-      <input type="text" id="getItemName" name="getItemName">
-      <button v-on:click="readItem">Lookup Item</button>
-    </div>
-    <div>
-      {{ getItemResults }}
+    <div class="container">
+      <h1>{{ msg }}</h1>
+      <p>
+        An application for managing an inventory database.
+      </p>
+      <div class="container mh-25">
+        <h3>Retrieve item(s) from the Item Database</h3>
+        <div class="inputs">
+          <label for="getItemName">Item name:</label>
+          <input type="text" id="getItemName" name="getItemName">
+          <button v-on:click="readItem">Lookup Item</button>
+        </div>
+        <div class="container overflow-scroll">
+          <table class="table table-striped table-bordered">
+            <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Type</th>
+              <th>Brand</th>
+              <th>Description</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in items" :key="item.id">
+              <td>{{ item.id }}</td>
+              <td>{{ item.name }}</td>
+              <td>{{ item.category }}</td>
+              <td>{{ item.type }}</td>
+              <td>{{ item.brand }}</td>
+              <td>{{ item.description }}</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-export default{
+
+export default {
   el: '#GIMS',
   props: {
     msg: String,
-    getItemResults: JSON
+    items: JSON
   },
   methods: {
-    readItem: function() {
+    readItem: function () {
       this.getItemResults = 'Loading...';
       let itemName = document.getElementById('getItemName').value;
       let vm = this;
       axios
           .get('http://localhost:8081/items/name/' + itemName)
-          .then(function(response){
-            vm.getItemResults = response.data;
+          .then(function (response) {
+            vm.items = response.data;
           })
-          .catch(function(error){
+          .catch(function (error) {
             vm.status = 'An error occurred' + error;
           })
     }
@@ -47,14 +73,17 @@ export default{
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
